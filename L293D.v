c93ffc51302f65b0b0dbd0192f5870860495c373
Ammar Ansari <107551808+capt-FROSTY@users.noreply.github.com>
2023-06-03 19:38:01 +0530

    Create L293D.v

---
+++ L293D.v
@@ -0,0 +1,30 @@
+module L293D(
+input in1, in2, in3, in4,
+input enA, enB,clk,
+output reg out1, out2, out3, out4 
+    );
+always@(posedge clk)
+begin
+  if(enA == 1)
+    begin
+      out1<=in1;
+      out2<=in2;
+    end 
+  else 
+    begin
+      out1<= 0;
+      out2<= 0;
+    end
+  
+  if(enB == 1)
+    begin
+      out3<=in3;
+      out4<=in4;  
+    end
+  else
+    begin
+      out3<= 0;
+      out4<= 0;
+    end
+end
+endmodule
